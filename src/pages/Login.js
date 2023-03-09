b387206b2a3f99b4d692437d2ae10e5f32f95d72
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-09 19:53:10 +0530

    Forms Hooks 1
    add react-toast-notifications for notifications

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,47 @@
+import { useState } from 'react';
 import styles from '../styles/login.module.css';
+import { useToasts } from 'react-toast-notifications';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
+
+  const handledFormSubmit = (e) => {
+    e.preventDefault();
+
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return addToast('Please enter both email and password', {
+        appearance: 'error',
+      });
+    }
+  };
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handledFormSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className={styles.field}>
-        <input type="password" placeholder="Password" required />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <div className={styles.field}>
-        <button>Login</button>
+      <div className={styles.field} disabled={loggingIn}>
+        <button>{loggingIn ? 'Logging In....' : 'Log In'}</button>
       </div>
     </form>
   );
