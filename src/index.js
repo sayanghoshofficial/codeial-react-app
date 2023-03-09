45a1488a82e928b1b11f1c082ef02ce662b611a3
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-09 20:32:58 +0530

    Auth Context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { ToastProvider } from 'react-toast-notifications';
 import './styles/index.css';
 import { App } from './components';
+import { AuthProvider } from './providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </React.StrictMode>
   </ToastProvider>
 );
